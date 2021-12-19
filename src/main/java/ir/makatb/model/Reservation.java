6a05180952e1824b5b394eea34b019f920c879d7
nasim <zamaninasim213@gamail.com>
2021-12-19 16:48:45 +0330

    add Reservation.java and annotation

---
+++ src/main/java/ir/makatb/model/Reservation.java
@@ -0,0 +1,22 @@
+package ir.makatb.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @OneToMany
+    private List<Ticket> tickets;
+    @ManyToOne
+    private Customer customer;
+    @CreationTimestamp
+    private Date reserveDate;
+}
