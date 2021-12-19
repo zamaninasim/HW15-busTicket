f65165f7d5fe6ce95ba73f2c5a49a47995842fe5
nasim <zamaninasim213@gamail.com>
2021-12-19 22:57:52 +0330

    add findByUsername method to AdminDao.java

--- src/main/java/ir/makatb/Main.java
+++ src/main/java/ir/makatb/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
 
 
+
                 break;
             case 2:
                 break;
--- src/main/java/ir/makatb/dao/AdminDao.java
+++ src/main/java/ir/makatb/dao/AdminDao.java
@@ -3,6 +3,10 @@ package ir.makatb.dao;
 import ir.makatb.model.Admin;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public class AdminDao extends BaseDao {
     public void save(Admin admin) {
@@ -11,5 +15,16 @@ public class AdminDao extends BaseDao {
         session.persist(admin);
         transaction.commit();
         session.close();
+    }
+
+    public Optional<Admin> findByUsername(String username) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Admin> query = session.createQuery("FROM Admin a WHERE a.username=:usernameValue");
+        query.setParameter("usernameValue", username);
+        Optional<Admin> admin = Optional.ofNullable(query.uniqueResult());
+        transaction.commit();
+        session.close();
+        return admin;
     }
 }
