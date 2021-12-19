df4179b0ed8790ee64f65c751cd9317948f2b30f
nasim <zamaninasim213@gamail.com>
2021-12-19 15:37:05 +0330

    modify customer class name to user

--- src/main/java/ir/makatb/model/User.java
+++ src/main/java/ir/makatb/model/User.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class Customer {
+public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstname;
